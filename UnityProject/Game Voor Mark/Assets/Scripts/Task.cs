0f54410792f32ea029d028985ce8df45dbb3ce4f
Corpelijn <bassiecorpelijn@hotmail.com>
2017-02-03 10:59:20 +0100

    First commit taskmanager

---
+++ UnityProject/Game Voor Mark/Assets/Scripts/Task.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    class Task : MonoBehaviour
+    {
+        #region "Fields"
+
+        public string Message;
+        public TaskState State;
+
+        #endregion
+
+        #region "Constructors"
+
+
+
+        #endregion
+
+        #region "Properties"
+
+
+
+        #endregion
+
+        #region "Methods"
+
+        public void Create(string message)
+        {
+            this.Message = message;
+            this.State = TaskState.Todo;
+        }
+
+        #endregion
+
+        #region "Abstract/Virtual Methods"
+
+
+
+        #endregion
+
+        #region "Inherited Methods"
+
+
+
+        #endregion
+
+        #region "Static Methods"
+
+
+
+        #endregion
+
+        #region "Operators"
+
+
+
+        #endregion
+    }
+}
---
+++ UnityProject/Game Voor Mark/Assets/Scripts/TaskManager.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    class TaskManager : MonoBehaviour
+    {
+        #region "Fields"
+
+        private List<Task> currentTasks;
+        private float pastTime;
+
+        public GameObject TaskPrefab = null;
+        public float SpawnTimer = 3f;
+
+        #endregion
+
+        #region "Constructors"
+
+
+
+        #endregion
+
+        #region "Singleton"
+
+        private static TaskManager instance;
+
+        public static TaskManager INSTANCE
+        {
+            get
+            {
+                return instance;
+            }
+        }
+
+        #endregion
+
+        #region "Properties"
+
+
+
+        #endregion
+
+        #region "Methods"
+
+        private void SpawnNewCard()
+        {
+            GameObject go = Instantiate(TaskPrefab);
+            Task t = go.GetComponent<Task>();
+            t.Create("test");
+
+            currentTasks.Add(t);
+        }
+
+        #endregion
+
+        #region "Abstract/Virtual Methods"
+
+
+
+        #endregion
+
+        #region "Inherited Methods"
+
+        public void Awake()
+        {
+            instance = this;
+
+            currentTasks = new List<Task>();
+        }
+
+        public void Update()
+        {
+            if (currentTasks.Count == 0 || pastTime >= SpawnTimer)
+            {
+                SpawnNewCard();
+
+                pastTime = 0;
+                return;
+            }
+
+            pastTime += Time.deltaTime;
+        }
+
+        #endregion
+
+        #region "Static Methods"
+
+
+
+        #endregion
+
+        #region "Operators"
+
+
+
+        #endregion
+    }
+}
