2655b34bc6ef5e2d34d1a28477aaafe5920e46a4
Maikel van der Velden <m.vdvelden@hotmail.com>
2017-02-03 14:20:45 +0100

    Added scene flow

---
+++ UnityProject/Game Voor Mark/Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour {
+
+    public void BackToStart()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
