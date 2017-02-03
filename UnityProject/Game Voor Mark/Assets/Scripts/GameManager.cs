3b28c9736ea58bb39ff917eb92f555a4cc63ee1d
Lorenzo <sheepytaggernep@gmail.com>
2017-02-03 10:43:02 +0100

    Added GameManager

---
+++ UnityProject/Game Voor Mark/Assets/Scripts/GameManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private bool _lost;
+
+    public float energyLeft = 30;
+
+
+    public void Archive()
+    {
+        if (_lost) return;
+        energyLeft += 5;
+    }
+
+    private void Update()
+    {        
+        energyLeft = Mathf.Max(0, energyLeft - (Time.deltaTime * 3));
+        if (energyLeft == 0)
+            Lose();
+    }
+
+    private void Lose()
+    {
+        _lost = true;
+        Debug.Log("You lose!");
+        enabled = false;
+    }
+
+}
